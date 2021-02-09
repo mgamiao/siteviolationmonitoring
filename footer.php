0c4188e97ad5a2a5a607c90d45a161bac2e7767c
Miguel Gamiao <miguelgamiao@gmail.com>
2021-02-09 14:57:38 +0800

    initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+ <!-- Footer -->
+ <footer class="sticky-footer bg-secondary text-light">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Miguel Gamiao | Gerald Balabis | Jerick Lara 2021</span>
+          </div>
+        </div>
+      </footer>
